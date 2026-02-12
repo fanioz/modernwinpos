4c2d81ac94eff98794c9907b4175962fd57564be
fanioz <1700770+fanioz@users.noreply.github.com>
2026-02-12 19:22:29 +0700

    Add project files.

---
+++ Imports.cs
@@ -0,0 +1,7 @@
+global using CommunityToolkit.Mvvm.ComponentModel;
+global using CommunityToolkit.Mvvm.Input;
+global using CommunityToolkit.Mvvm.Messaging;
+global using Microsoft.UI.Xaml;
+global using Microsoft.UI.Xaml.Controls;
+global using modernwinpos.ViewModels;
+global using modernwinpos.Views;
---
+++ ViewModels/BaseViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace modernwinpos.ViewModels
+{
+    public partial class BaseViewModel : ObservableObject
+    {
+        public BaseViewModel()
+        {
+
+        }
+
+        [ObservableProperty]
+        private string _title = string.Empty;
+    }
+}
